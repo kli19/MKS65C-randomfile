2b9ffcbf5ece2d30981bb405254601b88fa627f1
kli19 <kli19@stuy.edu>
2018-10-29 15:34:35 -0400

    close  and free

--- randomfile.c
+++ randomfile.c
@@ -7,12 +7,18 @@ int rand_num(){
   int random_data = open("/dev/random", O_RDONLY);
   int * p = (int *)malloc(sizeof(int));
   read(random_data, p, sizeof(p));
-  return *p;
+  int num = *p;
+  free(p);
+  if (close(random_data) < 0){
+    printf("ERROR CLOSING FILE");
+  };
+  return num;
 }
 
 int main(){
 
   printf("%d\n", rand_num());
+  
 
   return 0;
 }
